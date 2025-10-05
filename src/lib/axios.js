43d1366531809df4f95b1291af0cb8cb1d253a6b
unknown <phuongnguyen240805@gmail.com>
2025-10-05 13:09:42 +0700

    redeploy be

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://call-app-wheat.vercel.app/api";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://call-app-gfzb.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
